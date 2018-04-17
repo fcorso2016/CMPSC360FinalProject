aafb9fe5cbeeaba5600cbdba7b336bbfed62a153
fcorso2016 <fcorso2016@gmail.com>
2018-04-17 18:20:03 -0400

    Added Project

---
+++ src/MainWindow.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+/**
+ * This class is the main window that draws the graph and other elements onto the screen
+ */
+public class MainWindow {
+    private JPanel panel1;
+
+}
