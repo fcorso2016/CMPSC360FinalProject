a549c1e6bbea81b7ec6797c22514f05d73341a0b
fcorso2016 <fcorso2016@gmail.com>
2018-04-22 01:45:00 -0400

    Switched to Visual Studio C++ Project

--- src/MainWindow.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-/**
- * This class is the main window that draws the graph and other elements onto the screen
- */
-public class MainWindow {
-    private JPanel panel1;
-
-}
